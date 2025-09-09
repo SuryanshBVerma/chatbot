19a71e40a8d7331cf080b724eb54cc044b2cb45b
SuryanshBVerma <suryanshbverma@gmail.com>
2025-09-09 13:52:53 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+import AIAssistantUI from './components/AIAssistantUI'
+
+
+
+function App() {
+
+  return (
+    <>
+     <AIAssistantUI></AIAssistantUI>
+    </>
+  )
+}
+
+export default App
