2e7b3d83a2123671c210a0ba324c8d21f8c2e023
Suryansh Bachchan Verma <76619671+SuryanshBVerma@users.noreply.github.com>
2025-09-10 16:24:12 +0530

    Integrate react hot toast for notification services

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ToastContainer } from 'react-toastify';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ToastContainer
+      hideProgressBar={true}
+      theme={localStorage.getItem("theme")?.toString()}
+    />
     <App />
   </StrictMode>,
 )
